eafd1ef7285dae891ed3b0b1ef5cd0a6284619b4
Marco Muro <pgwall84@gmail.com>
2026-06-22 15:28:36 +0200

    Add bottom padding to dashboard for mobile scroll

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -69,7 +69,7 @@ export default function DashboardPage() {
   useEffect(() => { fetchPratiche(); }, [fetchPratiche]);
 
   return (
-    <div className="space-y-4">
+    <div className="space-y-4 pb-32">
       {/* Filtri + Esporta */}
       <div className="bg-white rounded-xl border border-gray-200 p-4 space-y-3">
         <input
